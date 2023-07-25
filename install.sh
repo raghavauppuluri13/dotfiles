709c733019a29fe916278c033f7749dc68b2746d
Raghava <raghava.upp13@gmail.com>
2023-07-25 10:00:36 -0400

    updates

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ mkdir -p $HOME/.vim/backup//
 # install
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-sudo apt install build-essential cmake vim-nox python3-dev
+sudo apt install build-essential cmake vim-nox python3-dev fzf silversearcher-ag -Y
 
 # copy files
 cp ./vimrc ~/.vimrc
@@ -18,3 +18,9 @@ cp ./tmux.conf ~/.tmux.conf
 # vim plugins + YCM 
 vim +PluginInstall +qall
 python3 ~/.vim/bundle/YouCompleteMe/install.py --clangd-completer --rust-completer
+
+# git configs
+
+git config --global user.name Raghava Uppuluri
+git config --global user.email raghava.upp13@gmail.com
+git config --global core.editor vim
